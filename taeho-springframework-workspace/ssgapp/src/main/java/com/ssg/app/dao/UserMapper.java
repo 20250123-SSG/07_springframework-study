1c16a99579da3047c80f70000528f538d8cd116e
THD <ok821000@gmail.com>
2025-05-16 08:56:12 +0900

    [Add] 종합앱 학습진행

--- taeho-springframework-workspace/ssgapp/src/main/java/com/ssg/app/dao/UserMapper.java
+++ taeho-springframework-workspace/ssgapp/src/main/java/com/ssg/app/dao/UserMapper.java
@@ -1,4 +1,14 @@
 package com.ssg.app.dao;
 
+
+import com.ssg.app.dto.UserDto;
+
 public interface UserMapper {
+    int insertUser(UserDto userDto);
+
+    int selectUserCountById(String checkId);
+
+    UserDto selectUserById(String userId);
+
+    int updateProfileImg(UserDto userDto);
 }
